af860ea69a86cf0d76ebc6e2d68e07aefc3e3cda
nasturtus <nasturtus@gmail.com>
2018-02-27 17:58:43 +0800

    Complete arrays and objectsassignments

--- 2-arrays.js
+++ 2-arrays.js
@@ -1,45 +1,106 @@
 var assertEquals = require("./test-helper");
 
-/* ---------------------- EXERCISE 1 ---------------------- */
-// Write a JavaScript function which accept a number as input and insert dashes (-) between each two even numbers. (Sample input: 025486, Sample output: 0-254-8-6)
-
-function addDashes(numString) {
-  return "0-254-8-6";
-}
-
-assertEquals(addDashes("025486"), "0-254-8-6"); // check that your function works as expected
-// assertEquals(dash('111246777'), '1112-4-6777') // uncomment this and run program. if test passes, uncomment subsequent tests one by one
-// assertEquals(dash('0021100'), '0-0-2110-0')
-// assertEquals(dash('your test input'), 'your expected output') // uncomment this line and add 2-3 more test cases. Do the same for all remaining functions
-
 /* ---------------------- EXERCISE 2 ---------------------- */
 // Write a Javascript function to find the most frequent item of an array. It should return a string denoting the item and the number of times it occurs in the array. (Sample input: [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3], expected output : 'a (5 times)')
 
-function mostFrequentItem(arr) {}
+function mostFrequentItem(arr) {
+  var maxCount = 0;
+  var maxChr = "";
+  var checkedChr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    chr = arr[i];
+    chrCount = 0;
+
+    if (checkedChr.findIndex(element => element === chr) === -1) {
+      for (var j = i; j < arr.length; j++) {
+        if (chr === arr[j]) {
+          chrCount++;
+        }
+      }
+      if (chrCount > maxCount) {
+        maxCount = chrCount;
+        maxChr = chr;
+      }
+
+      checkedChr.push(chr);
+    }
+  }
+  return maxChr;
+  //return maxChr + " (" + maxCount + " times)";
+}
 
 // uncomment the following test to run it
-// assertEquals(mostFrequentItem(['a', 'a', 'b']), 'a')
-// assertEquals(mostFrequentItem(['a', 'b', 'b', 'b', 'c', 'a', 'b', 'a', 'b']), 'b')
-// assertEquals(mostFrequentItem(['yes', 'yes', 'no', 'no', 'yes']), 'yes')
+//assertEquals(mostFrequentItem(["a", "a", "b"]), "a");
+//assertEquals(mostFrequentItem(["a", "b", "b", "b", "c", "a", "b", "a", "b"]),"b");
+//assertEquals(mostFrequentItem(["yes", "yes", "no", "no", "yes"]), "yes");
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 // Write a Javascript function to remove duplicate items from an array (ignore case sensitivity). (Sample input : [1, 'a', 'A', 'b', 2, 2], expected output: [1, 'a', 'b', 2])
-function removeDuplicateItems(arr) {}
+function allToLowerCase(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      arr[i] = arr[i].toLowerCase();
+    }
+  }
+  return arr;
+}
 
-// assertEquals(removeDuplicateItems([1, 1, 'a']), [1, 'a'])
-// assertEquals(removeDuplicateItems([1, 1, 'a', 'A']), [1, 'a'])
-// assertEquals(removeDuplicateItems([1, 'a', 'A', 'b', 'B', 2, 2]), [1, 'a', 'b', 2])
+function removeDuplicateItems(arr) {
+  var uniqItems = [];
 
-/* ---------------------- EXERCISE 4 ---------------------- */
-// Write a Javascript function to compute the union of two arrays. the returned array should be sorted in ascending order (Sample input: union([1, 2, 3], [100, 2, 1, 10]), expected output: [1, 2, 3, 10, 100])
-function union() {}
+  arr = allToLowerCase(arr);
 
-// assertEquals(union([1, 2], [2, 3]), [1, 2, 3])
-// assertEquals(union([1, 2, 3], [100, 2, 1, 10]), [1, 2, 3, 10, 100])
+  for (var i = 0; i < arr.length; i++) {
+    if (uniqItems.findIndex(element => element === arr[i]) === -1) {
+      uniqItems.push(arr[i]);
+    }
+  }
+  return uniqItems;
+}
 
-/* ---------------------- EXERCISE 5 ---------------------- */
-// Write a Javascript function to compute sum of a union. (Hint: Reuse the union() function which you wrote!) (Sample input: intersection([1, 2, 3], [1, 2]), expected output: 3)
-function unionSum() {}
-// assertEquals(unionSum([1, 2, 3], [1, 2]), 3)
-// assertEquals(unionSum([1, 2, 3, 4], [2, 3, 4]), 9)
-// assertEquals(unionSum([2000, 50, 551, 550, 23], [551, 50, 23]), 624)
+assertEquals(removeDuplicateItems([1, 1, "a"]), [1, "a"]);
+assertEquals(removeDuplicateItems([1, 1, "a", "A"]), [1, "a"]);
+assertEquals(removeDuplicateItems([1, "a", "A", "b", "B", 2, 2]), [
+  1,
+  "a",
+  "b",
+  2
+]);
+
+// ASSIGNMENTS FROM THE REVISED SET OF ARRAY ASSIGNMENTS
+
+/* ---------------------- EXERCISE 1 ---------------------- */
+// Define a function that returns the sum of an array
+function sum(array) {
+  var total = 0;
+  array.forEach(element => {
+    total += element;
+  });
+  return total;
+}
+
+assertEquals(sum([1, 2, 3, 4]), 10);
+assertEquals(sum([10, 10, 10]), 30);
+
+/* ---------------------- EXERCISE 2 ---------------------- */
+// Define a function that returns an array of even numbers
+function evenNumbersOnly(array) {
+  var evenNumbers = [];
+  evenNumbers = array.filter(number => number % 2 === 0);
+  return evenNumbers;
+}
+
+assertEquals(evenNumbersOnly([1, 2, 3, 4]), [2, 4]);
+assertEquals(evenNumbersOnly([10, 11, 12]), [10, 12]);
+
+/* ---------------------- EXERCISE 3 ---------------------- */
+// Define a function that returns the sum of even numbers in the array
+// hint: reuse the 2 functions that you defined earlier!
+function sumEvenNumbers(array) {
+  var evenNumbers = evenNumbersOnly(array);
+  return sum(evenNumbers);
+}
+
+assertEquals(sumEvenNumbers([1, 2, 3, 4]), 6);
+assertEquals(sumEvenNumbers([10, 11, 12]), 22);
--- 3-objects.js
+++ 3-objects.js
@@ -3,10 +3,19 @@ var assertEquals = require("./test-helper");
 /* ---------------------- EXERCISE 1 ---------------------- */
 // Define a function to list the properties of a JavaScript object. Go to the editor
 
-function listProperties() {}
+function listProperties(obj) {
+  str = "";
+  for (key in obj) {
+    str += key + " ";
+  }
+  return str.trim();
+}
 
-// assertEquals(listProperties({name: 'Tom Hanks'}), 'name')
-// assertEquals(listProperties({name: 'Tom Hanks', age: 50, rollno: 12}), 'name age rollno')
+assertEquals(listProperties({ name: "Tom Hanks" }), "name");
+assertEquals(
+  listProperties({ name: "Tom Hanks", age: 50, rollno: 12 }),
+  "name age rollno"
+);
 
 // Sample Output: name,sclass,rollno
 
@@ -31,26 +40,71 @@ var books = [
 ];
 
 // 2.1 Define a function listTitles(booksArray) that takes in an array of book objects and returns an array of titles (strings)
-function listTitles(booksArray) {}
+function listTitles(booksArray) {
+  var titles = [];
 
-// assertEquals(listTitles(books), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
+  booksArray.forEach(element => {
+    titles.push(element.title);
+  });
+  return titles;
+}
+
+assertEquals(listTitles(books), [
+  "The Road Ahead",
+  "Lord of the Rings",
+  "Harry Potter: The Prisoner of Azkaban"
+]);
 
 // 2.2 Define a function listAuthors(booksArray) that takes in an array of book objects and returns an array of authors (strings)
 
-function listAuthors(booksArray) {}
+function listAuthors(booksArray) {
+  var authors = [];
 
-// assertEquals(listTitles(books), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
+  booksArray.forEach(element => {
+    authors.push(element.author);
+  });
+  return authors;
+}
+
+assertEquals(listAuthors(books), ["Bill Gates", "JRR Tolkkien", "JK Rowling"]);
 
 // 2.3 Define a more general function listValues(booksArray, key) that takes in an array of book objects and returns an array of authors (strings)
 
-function listValues(booksArray, key) {}
+function listValues(booksArray, key) {
+  var arr = [];
 
-// assertEquals(listValues(books, 'author'), ['Bill Gates', 'JRR Tolkkien', 'JK Rowling'])
-// assertEquals(listValues(books, 'title'), ['The Road Ahead', 'Lord of the Rings', 'Harry Potter: The Prisoner of Azkaban'])
+  booksArray.forEach(obj => {
+    arr.push(obj[key]);
+  });
+  return arr;
+}
+
+assertEquals(listValues(books, "author"), [
+  "Bill Gates",
+  "JRR Tolkkien",
+  "JK Rowling"
+]);
+assertEquals(listValues(books, "title"), [
+  "The Road Ahead",
+  "Lord of the Rings",
+  "Harry Potter: The Prisoner of Azkaban"
+]);
 
 // 2.4 Define a function getAvailableBooks(booksArray) that returns a list of available books
 
-// assertEquals(getAvailableBooks(books), ['The Road Ahead', 'Lord of the Rings'])
+function getAvailableBooks(booksArray) {
+  var books = [];
+  booksArray.forEach(obj => {
+    books.push(obj.title);
+  });
+  return books;
+}
+
+assertEquals(getAvailableBooks(books), [
+  "The Road Ahead",
+  "Lord of the Rings",
+  "Harry Potter: The Prisoner of Azkaban"
+]);
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 /* 3.1 Define an object named fluffycat with the following properties/methods:
@@ -60,7 +114,18 @@ function listValues(booksArray, key) {}
     - introduceSelf() // return 'meow! my name is fluffy'
 */
 
-// assertEquals(fluffycat.name, "fluffy");
-// assertEquals(fluffycat.type, "pet");
-// assertEquals(fluffycat.makeSound(), "meow!");
-// assertEquals(fluffycat.introduceSelf(), "meow! my name is fluffy");
+var fluffycat = {
+  name: "fluffy",
+  type: "pet",
+  makeSound: function() {
+    return "meow!";
+  },
+  introduceSelf: function() {
+    return "meow! my name is fluffy";
+  }
+};
+
+assertEquals(fluffycat.name, "fluffy");
+assertEquals(fluffycat.type, "pet");
+assertEquals(fluffycat.makeSound(), "meow!");
+assertEquals(fluffycat.introduceSelf(), "meow! my name is fluffy");
